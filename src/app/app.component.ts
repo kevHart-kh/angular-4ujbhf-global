b58795ce3babb81533db4824ba7922793dab738a
kevHart-kh <c14180077@john.petra.ac.id>
2020-04-30 20:26:43 +0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobSerService } from './glob-ser.service';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  data : String;
+  dataskr : String = '';
+
+  constructor (public varglob : GlobSerService){
+    this.data = this.varglob.getData();
+  }
+
+  ubahData(){
+    this.varglob.setData(this.dataskr);
+    this.data = this.varglob.getData();
+  }
 }
--- src/app/glob-ser.service.ts
+++ src/app/glob-ser.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GlobSerService {
-  public Data = 'abc';
+  private Data = 'abc';
   constructor() { }
+
+  public getData(){
+    return this.Data;
+  }
+
+  public setData(dataBaru){
+    this.Data = dataBaru;
+  }
 
 }
